2723f56de361c26a72c20047f6bc4281919ebc67
lankai <lankai.yang@mail.utoronto.ca>
2025-09-30 15:04:46 -0400

    fix #3

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -165,6 +165,7 @@ public class NumberTriangle {
         return top;
     }
 
+
     public static void main(String[] args) throws IOException {
 
         NumberTriangle mt = NumberTriangle.loadTriangle("input_tree.txt");
